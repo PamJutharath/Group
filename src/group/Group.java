2782e28e800d5c4ec4574d3b1fea442bc3fada2a
PamJutharath <pam.btj.135@gmail.com>
2015-02-16 23:37:08 +0700

    update Group2
    ชื่อ

--- src/group/Group.java
+++ src/group/Group.java
@@ -5,6 +5,7 @@ public class Group {
 	public static void main(String[] args) {
 	     System.out.println("56051277 Jutharath");
 	     System.out.println("56051264 Kantida");
+	     System.out.println("56051257 Kamonwan");
 
 	}
 
